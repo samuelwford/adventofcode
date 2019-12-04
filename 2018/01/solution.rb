de3e2afe587f1583dfd85da5aef3db13c7eeb022
Samuel Ford <samuel.ford@disney.com>
2019-12-03 19:40:22 -0500

    add 2018 day 1 solution

--- 2018/01/solution.rb
+++ 2018/01/solution.rb
@@ -1,14 +1,39 @@
-file = ARGV[0] || "input.txt"
+file = "input.txt"
+# file = "test1.txt"
+# file = "test2.txt"
 
-lines = File.readlines(file).map { |x| x.to_i }
+dir = __dir__.nil? ? "" : __dir__ + "/"
+filename = dir + (ARGV[0] || file)
+
+dir # => ""
+filename # => "input.txt"
+
+lines = File.readlines(filename).map { |x| x.to_i }
 result = lines.reduce(0, :+)
+
+result # => 587
 
 puts "part one - #{result}"
 
 frequencies = [0]
+duplicates = []
 frequency = 0
 
-lines.each do |drift|
-  frequency += drift
+while duplicates.length == 0 do
+  lines.each do |drift|
+    frequency += drift
+
+    if frequencies.include?(frequency)
+      puts "seen #{frequency} before"
+      duplicates << frequency
+      break
+    end
   
+    frequencies << frequency
+  end
 end
+
+duplicates # => [83130]
+
+# >> part one - 587
+# >> seen 83130 before
