d8e7f7d039f09d082af2adfc98a5a2b178daabbc
Samuel Ford <samuel.ford@disney.com>
2019-12-04 10:47:00 -0500

    simplify some

--- 2019/04/p1.rb
+++ 2019/04/p1.rb
@@ -1,17 +1,28 @@
-input = "234208-765869" # !> assigned but unused variable - input
+input = "234208-765869".split('-')
+x = input[0].to_i
+y = input[1].to_i
+
+x # => 234208
+y # => 765869
+
+def has_doubles?(pwd)
+  pwd[0] == pwd[1] or pwd[1] == pwd[2] or pwd[2] == pwd[3] or pwd[3] == pwd[4] or pwd[4] == pwd[5]
+end
+
+has_doubles?("111111") # => true
+has_doubles?("223450") # => true
+has_doubles?("123789") # => false
+
+def ascending?(pwd)
+  pwd[5] >= pwd[4] and pwd[4] >= pwd[3] and pwd[3] >= pwd[2] and pwd[2] >= pwd[1] and pwd[1] >= pwd[0]
+end
+
+ascending?("111111") # => true
+ascending?("223450") # => false
+ascending?("123789") # => true
 
 def is_valid?(pwd)
-  ascending = true
-  has_doubles = false
-
-  last_digit = ""
-  pwd.each_char do |digit|
-    ascending = false if digit.to_i < last_digit.to_i
-    has_doubles = true if digit == last_digit
-    last_digit = digit
-  end
-  
-  ascending and has_doubles
+  has_doubles?(pwd) and ascending?(pwd)
 end
 
 def is_still_valid?(pwd)
@@ -22,16 +33,9 @@ def is_still_valid?(pwd)
   sets << [pwd[3..4], pwd[0..2] + pwd[5]]
   sets << [pwd[4..5], pwd[0..3]]
   
-  valid = false
-  sets.each do |set|
-    pair = set[0]
-    remainder = set[1]
-    if pair[0] == pair[1] and !remainder.include?(pair[0])
-      valid = true
-    end
-  end
+  valid_sets = sets.select { |p, r| p[0] == p[1] and !r.include?(p[0]) }
   
-  valid
+  valid_sets.length > 0
 end
 
 is_valid?("111111") # => true
@@ -43,7 +47,7 @@ is_still_valid?("123444") # => false
 is_still_valid?("111122") # => true
 
 candidates = []
-for i in 234208..768869
+for i in x..y
   candidates << i if is_valid?(i.to_s)
 end
 
@@ -52,3 +56,6 @@ candidates.length # => 1246
 filtered_candidates = candidates.select { |c| is_still_valid?(c.to_s) }
 
 filtered_candidates.length # => 814
+
+puts "#{candidates.length} candidates, #{filtered_candidates.length} after second pass"
+# >> 1246 candidates, 814 after second pass
--- 2019/04/readme.txt
+++ 2019/04/readme.txt
@@ -33,3 +33,7 @@ Given this additional criterion, but still ignoring the range rule, the followin
 How many different passwords within the range given in your puzzle input meet all of the criteria?
 
 Your puzzle input is still 234208-765869.
+
+Your puzzle answer was 814.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
