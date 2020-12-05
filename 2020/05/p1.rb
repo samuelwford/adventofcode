49712705ad8f3d133e81b9f043a37164934b5bd4
Samuel Ford <samuel.ford@icloud.com>
2020-12-05 09:14:19 -0500

    day 5

---
+++ 2020/05/p1.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby -w
+
+input = File.readlines(ARGV[0] || File.join(File.dirname(__FILE__), "input.txt"))
+
+def find_seat(code)
+  v, h = code[0..6], code[7..]
+  
+  r, c = 0, 0
+  
+  l, u = 0, 127
+  v.split("").each do |y|
+    m = (u - l).div(2)
+    l, u = l, u - m - 1 if y == "F"
+    l, u = l + m + 1, u if y == "B"
+    # puts "#{y} -> #{l}..#{u}"
+    r = l
+  end
+  
+  l, u = 0, 7
+  h.split("").each do |x|
+    m = (u - l).div(2)
+    l, u = l, u - m - 1 if x == "L"
+    l, u = l + m + 1, u if x == "R"
+    # puts "#{x} -> #{l}..#{u}"
+    c = l
+  end
+  
+  # puts "#{code}: (#{r}, #{c}) = #{r * 8 + c}"
+  
+  r * 8 + c
+end
+
+# puts "pass" if find_seat("FBFBBFFRLR") == 357
+# puts "pass" if find_seat("BFFFBBFRRR") == 567
+# puts "pass" if find_seat("FFFBBBFRRR") == 119
+# puts "pass" if find_seat("BBFFBBFRLL") == 820
+
+puts input.map { |code| find_seat(code) }.max()
