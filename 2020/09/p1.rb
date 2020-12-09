d15edb15c52db58402038c72412c33c4d9364415
Samuel Ford <samuel.ford@icloud.com>
2020-12-09 11:22:25 -0500

    experiement with fully functional solutions

--- 2020/09/p1.rb
+++ 2020/09/p1.rb
@@ -6,8 +6,17 @@ input.map! {|i| i.to_i}
 i = 25
 while i < input.length
   unless input[i - 25, 25].combination(2).map {|b| b[0] + b[1]}.include?(input[i])
-    puts input[i]
+    puts "found #{input[i]}"
     break
   end
   i += 1
 end
+
+# without a 'while'
+puts (0..input.length - 26)
+         .map { |i| [input[i + 25], input[i, 25].combination(2).map {|m,n| m+n}] }
+         .reject { |i,j| j.include? i }
+         .map { |i,j| i }
+
+# woah
+puts input[(0..input.length - 26).find { |i| !input[i, 25].combination(2).map {|m,n| m+n}.include?(input[i + 25]) } + 25]
