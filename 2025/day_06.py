f4433a7604aa140287c7fbc07c80fe449846a105
Samuel Ford <samuel.ford@icloud.com>
2025-12-14 09:49:31 -0500

    trying to get part 2

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -12,11 +12,11 @@ test_input = """
 """
 
 def parse(lines):
-	ops = list(lines.pop().split())
-	nums = [list(map(int, line.split())) for line in lines]
+	ops = list(lines[-1].split())
+	nums = [list(map(int, line.split())) for line in lines[:-1]]
 	pivoted = list(map(list, zip(*nums)))
 	return (pivoted, ops)
-	
+
 def part1(lines):
 	numbers, operators = parse(lines)
 	sum = 0
@@ -26,9 +26,45 @@ def part1(lines):
 		else:
 			sum += functools.reduce(operator.mul, numbers[i])
 	return sum
+
+def rotate_matrix(matrix):
+    return [''.join(row) for row in zip(*matrix)][::-1]
+
+def extract_columns(line, widths):
+    columns = []
+    index = 0
+    for width in widths:
+        columns.append(line[index:index+width])
+        index += width + 1
+    return columns
+
+def part2(lines):
+	p1, operators = parse(lines)
+	print(len(p1), len(lines))
+	widths = [len(str(max(p1[i]))) for i in range(len(p1))]
+	p2 = [extract_columns(l, widths) for l in lines]
+	print(len(p2))
+	p3 = list(zip(*p2))
+	p4 = [rotate_matrix(m) for m in p3]
+
+	total = 0
+	for i in range(len(p4)):
+		numbers = [int(i) for i in p4[i]]
+		if operators[i] == '+':
+			sum = functools.reduce(operator.add, numbers)
+		else:
+			sum = functools.reduce(operator.mul, numbers)
+		total += sum
+	return total
 	
 result = part1(test_input.strip().splitlines())
 assert result == 4277556, f"expected 4277556, got {result}"
 
 result = part1(utils.read_input('day_06.txt'))
 print('Part 1 -', result)
+
+result = part2(test_input.strip().splitlines())
+assert result == 3263827, f"expected 32638272, got {result}"
+
+result = part2(utils.read_input('day_06.txt'))
+print('Part 2 -', result)
